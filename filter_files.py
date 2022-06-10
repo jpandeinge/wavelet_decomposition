8ee057651b56efdbdd74673e6bd7fa6057d48b15
P55329CK <jpberbs@gmail.com>
2022-06-10 14:29:01 +0100

    filter files indices used in the testing set

--- filter_files.py
+++ filter_files.py
@@ -5,28 +5,23 @@ import pandas as pd
 
 # get path to all files
 path = 'data/spectra/simulated_data/'
+indices_path = 'data/params/'
 
-vlsr_list = []
+file_indices = pd.read_csv(indices_path + 'testing_data_indices.csv', header=None, skiprows=1)
+files = sorted(glob.glob(path + 'model_parameters_data*.txt'), key=lambda x: int(re.search(r'\d+', x).group()))
+
+
 filename_list = []
 
 def get_filenames(path):
     """
     Get the file names of the files in the directory.
     """
+    file_indices = pd.read_csv(indices_path + 'testing_data_indices.csv', header=None)
     files = sorted(glob.glob(path + 'model_parameters_data*.txt'), key=lambda x: int(re.search(r'\d+', x).group()))
-    for file in files:
-        data = pd.read_csv(file, sep='\t', header=None)
+    for file_index in file_indices[1]:
+        for index, file in enumerate(files):
+            if file_index == index:
+                filename_list.append(file.split('/')[-1])
 
-        # get the postive and negative float values of line 6 using regular expression
-        vlsr_list.append(float(re.findall(r'[+-]?\d+\.\d+', str(data.iloc[5, :]))[0]))
-
-        # append filename to list
-        filename_list.append(file.split('/')[-1])
-
-    # put the filename and vlsr values in a dataframe
-    vlsr_df = pd.DataFrame({'filename': filename_list, 'vlsr': vlsr_list})
-    vlsr_df = vlsr_df[vlsr_df['vlsr']>-60].reset_index(drop=True)
-    
-    filenames  = vlsr_df['filename']
-   
-    return filenames
+    return filename_list[1:]
