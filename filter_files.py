b16aa4b49938c65173baaa229186e48d5e2aa4b2
P55329CK <jpberbs@gmail.com>
2022-07-11 16:30:04 +0100

    refactor directory path

--- filter_files.py
+++ filter_files.py
@@ -4,21 +4,21 @@ import pandas as pd
 
 
 # get path to all files
-path = 'data/spectra/simulated_data/'
-indices_path = 'data/params/'
+PATH = 'data/synthetic/spectra/simulated_data/'
+INDICES_PATH = 'data/synthetic/params/'
 
-file_indices = pd.read_csv(indices_path + 'testing_data_indices.csv', header=None, skiprows=1)
-files = sorted(glob.glob(path + 'model_parameters_data*.txt'), key=lambda x: int(re.search(r'\d+', x).group()))
+file_indices = pd.read_csv(INDICES_PATH + 'testing_data_indices.csv', header=None, skiprows=1)
+files = sorted(glob.glob(PATH + 'model_parameters_data*.txt'), key=lambda x: int(re.search(r'\d+', x).group()))
 
 
 filename_list = []
 
-def get_filenames(path):
+def get_filenames(PATH):
     """
     Get the file names of the files in the directory.
     """
-    file_indices = pd.read_csv(indices_path + 'testing_data_indices.csv', header=None)
-    files = sorted(glob.glob(path + 'model_parameters_data*.txt'), key=lambda x: int(re.search(r'\d+', x).group()))
+    file_indices = pd.read_csv(INDICES_PATH + 'testing_data_indices.csv', header=None)
+    files = sorted(glob.glob(PATH + 'model_parameters_data*.txt'), key=lambda x: int(re.search(r'\d+', x).group()))
     for file_index in file_indices[1]:
         for index, file in enumerate(files):
             if file_index == index:
