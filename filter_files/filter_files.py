e0bf73601e80a0121982ec99b2ad704ac039a84c
P55329CK <jpberbs@gmail.com>
2022-08-25 12:14:26 +0100

    fix filtering of files names from training data

--- filter_files/filter_files.py
+++ filter_files/filter_files.py
@@ -7,47 +7,22 @@ import pandas as pd
 SIMULATED_FILES_PATH = '../data/synthetic/spectra/simulated_data/'
 INDICES_FILE_PATH = '../data/synthetic/params/'
 
-tree_based_file_indices_model = pd.read_csv(INDICES_FILE_PATH + 'testing_data_indices.csv', header=None, skiprows=1)
-nn_based_file_indices_model = pd.read_csv(INDICES_FILE_PATH + 'nn_testing_data_indices.csv', header=None, skiprows=1)
-
+file_indices = pd.read_csv(INDICES_FILE_PATH + 'testing_data_indices.csv', header=None, skiprows=1)
+# nn_based_file_indices_model = pd.read_csv(INDICES_FILE_PATH + 'nn_testing_data_indices.csv', header=None, skiprows=1)
 
 files = sorted(glob.glob(SIMULATED_FILES_PATH + 'model_parameters_data*.txt'), key=lambda x: int(re.search(r'\d+', x).group()))
 
-
 filename_list = []
 
-def get_filenames(SIMULATED_FILES_PATH, model_type):
+def get_filenames(path):
     """
     Get the file names of the files in the directory.
     """
-    if model_type == 'tree':
-        filename_list.clear() # clear the list
-        tree_file_indices = tree_based_file_indices_model
-        for file_index in tree_file_indices[1]:
-            for index, file in enumerate(files):
-                if file_index == index:
-                    filename_list.append(file.split('/')[-1])
-    
-    elif model_type == 'nn':
-        filename_list.clear() # clear the list
-        nn_file_indices = nn_based_file_indices_model
-        for file_index in nn_file_indices[1]:
-            for index, file in enumerate(files):
-                if file_index == index:
-                    filename_list.append(file.split('/')[-1])
+    file_indices = pd.read_csv(INDICES_FILE_PATH + 'testing_data_indices.csv', header=None)
+    files = sorted(glob.glob(path + 'model_parameters_data*.txt'), key=lambda x: int(re.search(r'\d+', x).group()))
+    for file_index in file_indices[1]:
+        for index, file in enumerate(files):
+            if file_index == index:
+                filename_list.append(file.split('/')[-1])
 
-    elif model_type == 'lte':
-        filename_list.clear() # clear the list
-        tree_file_indices = tree_based_file_indices_model
-        nn_file_indices = nn_based_file_indices_model
-
-        list_of_model_file_indices = [tree_file_indices, nn_file_indices]
-        for model_file in list_of_model_file_indices:
-            for file_index in model_file[1]:
-                for index, file in enumerate(files):
-                    if file_index == index:
-                        filename_list.append(file.split('/')[-1])
-    else:
-        raise ValueError('model_type must be either "lte", "tree" or  "nn"')
-    
-    return filename_list[:]
+    return filename_list[1:] 
